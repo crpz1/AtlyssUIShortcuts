0f8d74f08fbd15ace5b41481f1c2cde7b3f86445
crpz1 <8588315+crpz1@users.noreply.github.com>
2024-12-14 22:27:11 +1100

    Accept/Deny invite shortcuts

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,36 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
+using Object = System.Object;
 
-namespace AtlyssItemTweaks
+namespace AtlyssUIShortcuts
 {
-    public class AtlyssItemTweaks : BaseUnityPlugin
+    [BepInPlugin("crpz.AtlyssUIShortcuts", "AtlyssUIShortcuts", "1.0.0")]
+    public class AtlyssUIShortcuts : BaseUnityPlugin
     {
+        internal new static ManualLogSource Logger { get; private set; } = null;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            //Harmony = new Harmony("crpz.AtlyssUIShortcuts");
+            //Harmony.PatchAll();
+            Logger.Log(LogLevel.Message, "hii :3");
+        }
+
+        private void Update()
+        {
+            //Logger.Log(LogLevel.Message, Cursor.lockState);
+            if (AtlyssNetworkManager._current == null || PartyUIManager._current == null) return;
+            if (AtlyssNetworkManager._current._soloMode) return;
+            if (PartyUIManager._current._partyObject != null) return;
+            if (PartyUIManager._current._partyInviteElement.isEnabled == false) return;
+            if (Input.GetKeyDown(KeyCode.Y)) Player._mainPlayer.Cmd_SetPartyInviteCondition(PartyInviteStatus.ACCEPTED);
+            if (Input.GetKeyDown(KeyCode.N)) Player._mainPlayer.Cmd_SetPartyInviteCondition(PartyInviteStatus.DECLINED);
+
+        }
     }
 }
